301e0f147ef86808f528c6c793d7af93749c7277
Farhad9790 <farhadatakishiyev2010@gmail.com>
2026-09-03 18:56:58 +0400

    Implement mydriver initialization and exit functions
    Initialize mydriver module with LZMA support logging.

--- drivers/mydriver/mydriver.c
+++ drivers/mydriver/mydriver.c
@@ -1,1 +1,20 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/decompress/unlzma.h>
 
+static int __init mydriver_init(void)
+{
+    printk(KERN_INFO "MyDriver: Loading on zepharo...\n");
+    printk(KERN_INFO "MyDriver: LZMA support is ready.\n");
+    return 0;
+}
+
+static void __exit mydriver_exit(void)
+{
+    printk(KERN_INFO "MyDriver: Unloading...\n");
+}
+
+module_init(mydriver_init);
+module_exit(mydriver_exit);
+MODULE_LICENSE("GPL");
